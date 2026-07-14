5248681b99d8be7e59df3e50ef8664b2ec75f8ae
dingkhann-blip <dingkhann@gmail.com>
2026-07-14 23:06:57 +0430

    Create perfume.js

---
+++ perfume.js
@@ -0,0 +1,26 @@
+const PERFUMES = [
+  { id: 1,  name: "Rose Perfume",          size: "50 mL", price: 20 },
+  { id: 2,  name: "Waqar Special Perfume", size: "50 mL", price: 25 },
+  { id: 3,  name: "Royal Oud",             size: "50 mL", price: 30 },
+  { id: 4,  name: "Midnight Musk",         size: "50 mL", price: 28 },
+  { id: 5,  name: "Golden Amber",          size: "50 mL", price: 27 },
+  { id: 6,  name: "Jasmine Dream",         size: "50 mL", price: 22 },
+  { id: 7,  name: "Ocean Breeze",          size: "50 mL", price: 24 },
+  { id: 8,  name: "Velvet Rose",           size: "50 mL", price: 26 },
+  { id: 9,  name: "Black Orchid",          size: "50 mL", price: 32 },
+  { id: 10, name: "White Pearl",           size: "50 mL", price: 23 },
+  { id: 11, name: "Desert Sandal",         size: "50 mL", price: 29 },
+  { id: 12, name: "Fresh Citrus",          size: "50 mL", price: 21 },
+  { id: 13, name: "Silver Mist",           size: "50 mL", price: 24 },
+  { id: 14, name: "Night Elegance",        size: "50 mL", price: 31 },
+  { id: 15, name: "Waqar Elite",           size: "50 mL", price: 35 }
+];
+
+const BUSINESS = {
+  name: "Waqar Perfume",
+  whatsappNumber: "93568761287",       // international format, no + or spaces, used for wa.me links
+  whatsappDisplay: "0568761287",
+  country: "Afghanistan",
+  delivery: "Free delivery across Afghanistan",
+  payments: ["Bank Transfer", "Cash on Delivery"]
+};
